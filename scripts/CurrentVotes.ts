f8912d9f1ad52a2d28cc722b29f188fb25618447
dex85 <alexander.keller.85@web.de>
2024-03-24 20:45:32 -0700

    add functionality

---
+++ scripts/CurrentVotes.ts
@@ -0,0 +1,39 @@
+import { abi, bytecode } from "../artifacts/contracts/Ballot.sol/Ballot.json";
+import { createPublicClient, http, formatEther, createWalletClient, toHex, hexToString } from "viem";
+import { sepolia } from "viem/chains";
+import { privateKeyToAccount } from "viem/accounts";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const providerApiKey = process.env.ALCHEMY_API_KEY || "";
+
+async function main() {
+    const parameters = process.argv.slice(2);
+    if (!parameters || parameters.length < 2)
+      throw new Error("Parameters not provided\nParam1: contract address\nParam2: proposalnumber");
+      const publicClient = createPublicClient({
+        chain: sepolia,
+        transport: http(`https://eth-sepolia.g.alchemy.com/v2/${providerApiKey}`),
+      });
+    
+    const contractAddress = parameters[0] as `0x${string}`;
+    const proposal = parameters[1];
+
+    if (!contractAddress) throw new Error("Contract address not provided");
+    if (!/^0x[a-fA-F0-9]{40}$/.test(contractAddress))
+      throw new Error("Invalid contract address");
+
+    const result = await publicClient.readContract({
+        address: contractAddress,
+        abi,
+        functionName: "proposals",
+        args: [proposal],
+      }) as any[];
+      console.log(`Proposal ${proposal} has ${result[1]} votes so far.`);
+    process.exit();
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
